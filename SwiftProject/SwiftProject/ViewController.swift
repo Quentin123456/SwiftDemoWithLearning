abc87c9c77daa055e18c56fef4285d0fb7a34e6d
Quentin zang <quentinzang@gmail.com>
2020-01-07 21:26:45 +0800

    closure闭包

--- SwiftProject/SwiftProject/ViewController.swift
+++ SwiftProject/SwiftProject/ViewController.swift
@@ -46,6 +46,7 @@ class ViewController: UIViewController {
         stringBtn.backgroundColor = .blue
         stringBtn.layer.masksToBounds = true
         stringBtn.layer.cornerRadius = 8
+        stringBtn.titleLabel?.font = UIFont.systemFont(ofSize: 15)
         stringBtn.tag = 0x100000
         stringBtn.addTarget(self, action: #selector(buttonClicked(_sender:)), for: .touchUpInside)
         contentView.addSubview(stringBtn)
@@ -62,11 +63,28 @@ class ViewController: UIViewController {
               make.top.equalTo(contentView).offset(64 + 20 * screenScale)
             }
 
-               make.left.equalTo(20 * screenScale)
-               make.height.equalTo(30 * screenScale)
-               make.right.equalTo(-20 * screenScale)
-               make.bottom.equalToSuperview().offset(-10 * screenScale)
+               make.left.equalTo(30 * screenScale)
+               make.height.equalTo(25 * screenScale)
+               make.right.equalTo(-30 * screenScale)
+              
            }
+        
+        let summaryBtn = UIButton(type: .custom)
+        summaryBtn.setTitle("总结", for: .normal)
+        summaryBtn.backgroundColor = .gray
+        summaryBtn.tag = 0x100001
+        summaryBtn.layer.masksToBounds = true
+        summaryBtn.layer.cornerRadius = 8
+        summaryBtn.titleLabel?.font = UIFont.systemFont(ofSize: 15)
+        summaryBtn.addTarget(self, action: #selector(buttonClicked(_sender:)), for: .touchUpInside)
+        contentView.addSubview(summaryBtn)
+        summaryBtn.snp.makeConstraints { (make) in
+            make.top.equalTo(stringBtn.snp.bottom).offset(10 * screenScale)
+            make.height.right.left.equalTo(stringBtn)
+             make.bottom.equalToSuperview().offset(-10 * screenScale)//必须加这一句最后，否则scrollView不起效果
+        }
+        
+        
     }
     
     @objc func buttonClicked(_sender:UIButton) {
@@ -74,6 +92,9 @@ class ViewController: UIViewController {
             let stringVC = StringUsageViewController()
             self.navigationController?.pushViewController(stringVC, animated: true)
             //self.present(stringVC, animated: true, completion: nil)
+        } else if _sender.tag == 0x100001 {
+            let summaryVC = SummaryViewController()
+            self.navigationController?.pushViewController(summaryVC, animated: true)
         }
     }
 
