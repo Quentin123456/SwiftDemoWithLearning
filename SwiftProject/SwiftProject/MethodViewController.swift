e0c24f4207755dc79da47aad1a94802e5fa523c0
QuentinZang <quentinzang@outlook.com>
2020-01-26 20:25:37 +0800

    方法和下标

--- SwiftProject/SwiftProject/MethodViewController.swift
+++ SwiftProject/SwiftProject/MethodViewController.swift
@@ -17,9 +17,142 @@ class MethodViewController: UIViewController {
         self.navigationController?.navigationBar.topItem?.title = ""
         self.title = "方法"
         self.view.backgroundColor = .white
+        
+        print("-------------------------")
+        let counter = Counter()
+        // the initial counter value is 0
+        counter.increment()
+        // the counter's value is now 1
+        counter.increment(by: 5)
+        // the counter's value is now 6
+        counter.reset()
+        // the counter's value is now 0
+        print(counter)
+        
+        print("-------------------------")
+        var somePoint = Point(x: 1.0, y: 1.0)
+        somePoint.moveBy(x: 2.0, y: 3.0)
+        print("The point is now at (\(somePoint.x), \(somePoint.y))")
+        // Prints "The point is now at (3.0, 4.0)"
+        
+        print("-------------------------")
+        if somePoint.isToTheRightOf(x: 1.0) {
+            print("This point is to the right of the line where x == 1.0")
+        }
+        // Prints "This point is to the right of the line where x == 1.0"
+        
+        print("-------------------------")
+        var ovenLight = TriStateSwitch.low
+        ovenLight.next()
+        // ovenLight is now equal to .high
+        ovenLight.next()
+        // ovenLight is now equal to .off
+        print(ovenLight)
+        
+        print("-------------------------")
+        var player = Player(name: "Argyrios")
+        player.complete(level: 1)
+        print("highest unlocked level is now \(LevelTracker.highestUnlockedLevel)")
+        // Prints "highest unlocked level is now 2"
+        
+        print("-------------------------")
+        player = Player(name: "Beto")
+        if player.tracker.advance(to: 6) {
+            print("player is now on level 6")
+        } else {
+            print("level 6 has not yet been unlocked")
+        }
+        // Prints "level 6 has not yet been unlocked"
     }
     
+    
+    // MARK: - Instance Methods -- 实例方法
+    class Counter {
+        var count = 0
+        func increment() {
+            count += 1
+        }
+        
+        func increment(by amount: Int) {
+            count += amount
+        }
+        
+        func reset() {
+            count = 0
+        }
+    }
+    
+    struct Point {
+        var x = 0.0, y = 0.0
+        func isToTheRightOf(x: Double) -> Bool {
+            return self.x > x
+        }
+        //从实例方法中修改值类型
+        mutating func moveBy(x deltaX: Double, y deltaY: Double) {
+            x += deltaX
+            y += deltaY
+        }
+    }
+    
+    //在变异方法中分配给自身
+//    struct Point {
+//        var x = 0.0, y = 0.0
+//        mutating func moveBy(x deltaX: Double, y deltaY: Double) {
+//            self = Point(x: x + deltaX, y: y + deltaY)
+//        }
+//    }
 
+    enum TriStateSwitch {
+        case off, low, high
+        mutating func next() {
+            switch self {
+            case .off:
+                self = .low
+            case .low:
+                self = .high
+            case .high:
+                self = .off
+            }
+        }
+    }
+    
+    
+    // MARK: - Type Methods  -- 类型方法
+    struct LevelTracker {
+        static var highestUnlockedLevel = 1
+        var currentLevel = 1
+
+        static func unlock(_ level: Int) {
+            if level > highestUnlockedLevel { highestUnlockedLevel = level }
+        }
+
+        static func isUnlocked(_ level: Int) -> Bool {
+            return level <= highestUnlockedLevel
+        }
+
+        @discardableResult
+        mutating func advance(to level: Int) -> Bool {
+            if LevelTracker.isUnlocked(level) {
+                currentLevel = level
+                return true
+            } else {
+                return false
+            }
+        }
+    }
+    
+    class Player {
+        var tracker = LevelTracker()
+        let playerName: String
+        func complete(level: Int) {
+            LevelTracker.unlock(level + 1)
+            tracker.advance(to: level + 1)
+        }
+        init(name: String) {
+            playerName = name
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
--- SwiftProject/SwiftProject/SubscriptsViewController.swift
+++ SwiftProject/SwiftProject/SubscriptsViewController.swift
@@ -17,9 +17,78 @@ class SubscriptsViewController: UIViewController {
         self.navigationController?.navigationBar.topItem?.title = ""
         self.title = "下标"
         self.view.backgroundColor = .white
+        
+        print("-------------------------")
+        let threeTimesTable = TimesTable(multiplier: 3)
+        print("six times three is \(threeTimesTable[6])")
+        // Prints "six times three is 18"
+        
+        //下标用法
+        print("-------------------------")
+        var numberOfLegs = ["spider": 8, "ant": 6, "cat": 4]
+        numberOfLegs["bird"] = 2
+        print(numberOfLegs)
+        
+        print("-------------------------")
+        var matrix = Matrix(rows: 2, columns: 2)
+        matrix[0, 1] = 1.5
+        matrix[1, 0] = 3.2
+        print(matrix)
+        
+        //触发断言
+//        print("-------------------------")
+//        let someValue = matrix[2, 2]
+//        // This triggers an assert, because [2, 2] is outside of the matrix bounds.
+//        print(someValue)
+        
+        print("-------------------------")
+        let mars = Planet[4]
+        print(mars)
     }
     
+    
+    struct TimesTable {
+        let multiplier: Int
+        subscript(index: Int) -> Int {
+            return multiplier * index
+        }
+    }
 
+    // MARK: - Subscript Options -- 下标选项
+    struct Matrix {
+        let rows: Int, columns: Int
+        var grid: [Double]
+        init(rows: Int, columns: Int) {
+            self.rows = rows
+            self.columns = columns
+            grid = Array(repeating: 0.0, count: rows * columns)
+        }
+        
+        func indexIsValid(row: Int, column: Int) -> Bool {
+            return row >= 0 && row < rows && column >= 0 && column < columns
+        }
+        
+        subscript(row: Int, column: Int) -> Double {
+            get {
+                assert(indexIsValid(row: row, column: column), "Index out of range")
+                return grid[(row * columns) + column]
+            }
+            set {
+                assert(indexIsValid(row: row, column: column), "Index out of range")
+                          grid[(row * columns) + column] = newValue
+            }
+        }
+        
+    }
+    
+    // MARK: - Type Subscripts -- 类型下标
+    enum Planet: Int {
+        case mercury = 1, venus, earth, mars, jupiter, saturn, uranus, neptune
+        static subscript(n: Int) -> Planet {
+            return Planet(rawValue: n)!
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
